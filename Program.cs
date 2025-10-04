a5f4ca13ac0c4d264d096bfda7316ca00f6fc06f
249829 <249829@KB7-118-C2>
2025-10-04 13:13:34 +0300

    Создан класс и перчисление категорий

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum ItemCategory
+{
+    Domestic,
+    Childhood,
+    Food
+}
+public class Item
+{
+    public int Id;
+    public string Name;
+    public decimal Price;
+    public int Quantity;
+    public bool Warehouse;
+    public ItemCategory Category;
+
+}
+
+// Методы должны работать со списком (снаружи класса)
